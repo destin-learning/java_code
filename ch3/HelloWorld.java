07ea6135588361ef7d7f45efe26aac893a50ee9f
erfrick <47677245+erfrick@users.noreply.github.com>
2020-05-15 15:27:10 -0400

    Create HelloWorld.java

---
+++ ch3/HelloWorld.java
@@ -0,0 +1,11 @@
+// chapter 3
+public class HelloWorld 
+{
+    public static void main(final String[] args) {
+
+        // this is my first java program
+        System.out.println("Hello World");
+
+    }
+
+}
