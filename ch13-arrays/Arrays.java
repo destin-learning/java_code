a15bbd0a9015ad28eeb284f19b59143fae8cdd9c
Eric Frick <efrick@destinlearning..com>
2020-07-08 14:16:20 -0400

    Added Arrays program for Chapter 13

---
+++ ch13-arrays/Arrays.java
@@ -0,0 +1,14 @@
+// chapter 13 Arrays
+public class Arrays 
+{
+    public static void main(final String[] args) {
+
+        // initialize the array
+        String [] teams = {"Buckeyes", "Badgers", "Hoosiers", "Illini", "Wolverines"};
+
+        // Print out the first team in the array
+        System.out.println("The first team is: " + teams[0]);
+
+    }
+
+}
